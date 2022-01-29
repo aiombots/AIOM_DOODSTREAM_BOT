c4b1c75fdd562cedc35b8dea24bf32cc3589b7a3
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-01-29 14:41:38 +0530

    Update start_help.py

--- plugins/start_help.py
+++ plugins/start_help.py
@@ -13,5 +13,5 @@ async def start_message(bot, message):
 
 @app.on_message(filters.command("help"))
 async def help_message(bot, message):
-    await message.reply_text("Hᴏᴡ Tᴏ Usᴇ Mᴇ?\n\n1. Aᴅᴅ Yᴏᴜʀ Aᴘɪ Kᴇʏ Bʏ Usɪɴɢ /add Aᴘɪᴋᴇʏ\n(Gᴇɴᴇʀᴀᴛᴇ Yᴏᴜʀ Aᴘɪ Kᴇʏ Fʀᴏᴍ Hᴇʀᴇ).\n\n2.Nᴏᴡ Sᴇɴᴅ Aɴʏ Dɪʀᴇᴄᴛ Oʀ Rᴀᴡ Uʀʟ Oꜰ Aɴʏ Vɪᴅᴇᴏ\n\n(Iꜰ Yᴏᴜ Wᴀɴᴛ Tᴏ Aᴅᴅ Aɴʏ Cᴜsᴛᴏᴍ Nᴀᴍᴇ Fᴏʀ Vɪᴅᴇᴏ Tʜᴇɴ Sᴇɴᴅ Tʜᴀᴛ Cᴜsᴛᴏᴍ Nᴀᴍᴇ Aʟᴏɴɢ Wɪᴛʜ Uʀʟ Sᴇᴘᴀʀᴀᴛᴇᴅ Bʏ | ᴇ.ɢ., https://example.com/file.mp4 | Cᴜsᴛᴏᴍ Nᴀᴍᴇ)\n\n3.Iꜰ Yᴏᴜ Wᴀɴᴛ Tᴏ Rᴇᴍᴏᴠᴇ Yᴏᴜʀ Aᴘɪ Kᴇʏ Fʀᴏᴍ Bᴏᴛ Tʜᴇɴ Usᴇ /revoke")
+    await message.reply_text("Hᴏᴡ Tᴏ Usᴇ Mᴇ?\n\n1. Aᴅᴅ Yᴏᴜʀ Aᴘɪ Kᴇʏ Bʏ Usɪɴɢ /add Aᴘɪᴋᴇʏ\n(<a href='https://doodstream.com/settings'>Gᴇɴᴇʀᴀᴛᴇ Yᴏᴜʀ Aᴘɪ Kᴇʏ Fʀᴏᴍ Hᴇʀᴇ</a>).\n\n2.Nᴏᴡ Sᴇɴᴅ Aɴʏ Dɪʀᴇᴄᴛ Oʀ Rᴀᴡ Uʀʟ Oꜰ Aɴʏ Vɪᴅᴇᴏ\n\n(Iꜰ Yᴏᴜ Wᴀɴᴛ Tᴏ Aᴅᴅ Aɴʏ Cᴜsᴛᴏᴍ Nᴀᴍᴇ Fᴏʀ Vɪᴅᴇᴏ Tʜᴇɴ Sᴇɴᴅ Tʜᴀᴛ Cᴜsᴛᴏᴍ Nᴀᴍᴇ Aʟᴏɴɢ Wɪᴛʜ Uʀʟ Sᴇᴘᴀʀᴀᴛᴇᴅ Bʏ | ᴇ.ɢ., https://example.com/file.mp4 | Cᴜsᴛᴏᴍ Nᴀᴍᴇ)\n\n3.Iꜰ Yᴏᴜ Wᴀɴᴛ Tᴏ Rᴇᴍᴏᴠᴇ Yᴏᴜʀ Aᴘɪ Kᴇʏ Fʀᴏᴍ Bᴏᴛ Tʜᴇɴ Usᴇ /revoke\n\nIꜰ Fᴀᴄɪɴɢ Aɴʏ Pʀᴏʙʟᴇᴍ Tʜᴇɴ Asᴋ Aᴛ : @AIOM_BOTS")
 
