7006663f828e45779bca37e607cf614e3df512f1
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-01-29 15:05:21 +0530

    Update start_help.py

--- plugins/start_help.py
+++ plugins/start_help.py
@@ -11,7 +11,14 @@ from main import Main
 async def start_message(bot, message):
     await message.reply_text(
         text="Hᴇʟʟᴏ 👋,\n\nTʜɪs Is A Dᴏᴏᴅ Sᴛʀᴇᴀᴍ Uᴘʟᴏᴀᴅᴇʀ Bᴏᴛ\n\nYᴏᴜ Cᴀɴ Uᴘʟᴏᴀᴅ Vɪᴅᴇᴏ Fʀᴏᴍ Dɪʀᴇᴄᴛ/Rᴀᴡ Uʀʟ Tᴏ Dᴏᴏᴅ Dᴛʀᴇᴀᴍ\n\nCʟɪᴄᴋ Oɴ /help Fᴏʀ Mᴏʀᴇ Dᴇᴛᴀɪʟs\n\nPᴏᴡᴇʀᴅ Bʏ : @AIOM_BOTS",
-        replay_markup=
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("", url=""),
+            InlineKeyboardButton("", url="")
+            ],[
+            InlineKeyboardButton("", url="")
+            ]]
+            )
+       )
 
 @app.on_message(filters.command("help"))
 async def help_message(bot, message):
