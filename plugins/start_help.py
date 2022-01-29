e5b87774f02a9a5b854a4d996d065fdfa3b72f3e
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-01-29 12:52:26 +0530

    Update start_help.py

--- plugins/start_help.py
+++ plugins/start_help.py
@@ -7,5 +7,11 @@ from pyrogram.types import Message
 from main import Main
 
 
+@app.on_message(filters.command("start"))
+async def start_message(bot, message):
+    await message.reply_text("")
 
+@app.on_message(filters.command("help"))
+async def help_message(bot, message):
+    await message.reply_text("")
 
